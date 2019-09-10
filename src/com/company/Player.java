1789d6656ed93b09adeec4709b276fda42ad4067
Kurmanjan <54546665+Kurmanjan@users.noreply.github.com>
2019-09-10 18:37:19 +0300

    Update Player.java

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,23 +1,6 @@
 package com.company;
 
 public class Player {
-    private int age;
-    private String color;
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
+    
 }
 
