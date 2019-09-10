63ed7dee52be7ded1e86545c34f6fb51ea6a6914
Kurmanjan <54546665+Kurmanjan@users.noreply.github.com>
2019-09-10 18:36:24 +0300

    Update Boss.java

--- src/com/company/Boss.java
+++ src/com/company/Boss.java
@@ -1,23 +1,6 @@
 package com.company;
 
 public class Boss extends Player {
-    private String type;
-    private String breed;
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getBreed() {
-        return breed;
-    }
-
-    public void setBreed(String breed) {
-        this.breed = breed;
-    }
+      
 }
 
